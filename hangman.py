e32ce749fa926277aa3a36257a90bf148abc62c2
tagboto <zoe2.tagboto@gmail.com>
2017-09-22 20:24:34 +0000

    modified to run using python3

--- hangman.py
+++ hangman.py
@@ -1,17 +1,18 @@
+#Edited to run in python3
 #importing the time module
 import time
 
 #welcoming the user
-name = raw_input("What is your name? ")
+name = input("What is your name? ")
 
-print "Hello, " + name, "Time to play hangman!"
+print("Hello, " + name, "Time to play hangman!")
 
-print ""
+print("")
 
 #wait for 1 second
 time.sleep(1)
 
-print "Start guessing..."
+print("Start guessing...")
 time.sleep(0.5)
 
 #here we set the secret
@@ -38,12 +39,12 @@ while turns > 0:
         if char in guesses:    
     
         # print then out the character
-            print char,    
+            print(char)    
 
         else:
     
         # if not found, print a dash
-            print "_",     
+            print("_")     
        
         # and increase the failed counter with one
             failed += 1    
@@ -52,7 +53,7 @@ while turns > 0:
 
     # print You Won
     if failed == 0:        
-        print "You won"  
+        print("You won")  
 
     # exit the script
         break              
@@ -60,7 +61,7 @@ while turns > 0:
     print
 
     # ask the user go guess a character
-    guess = raw_input("guess a character:") 
+    guess = input("guess a character:") 
 
     # set the players guess to guesses
     guesses += guess                    
@@ -72,13 +73,13 @@ while turns > 0:
         turns -= 1        
  
     # print wrong
-        print "Wrong"    
+        print("Wrong")  
  
     # how many turns are left
-        print "You have", + turns, 'more guesses' 
+        print("You have", + turns, 'more guesses')
  
     # if the turns are equal to zero
         if turns == 0:           
     
-        # print "You Loose"
-            print "You Loose"  
+        # print "You Lose"
+            print("You Lose")  
